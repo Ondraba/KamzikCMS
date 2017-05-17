ae1013a03f0563e40bb12585147c6b68b24822a1
baska <ondra.basista@gmail.com>
2017-05-17 22:46:49 +0200

    issue#2packageSystemCoreLogic

--- Kamzik/Models/PageSystem/Packages.cs
+++ Kamzik/Models/PageSystem/Packages.cs
@@ -7,5 +7,14 @@ namespace Kamzik.Models.PageSystem
 {
     public class Packages
     {
+        public List<KeyValuePair<string, string>> page()
+        {
+            var package = new List<KeyValuePair<string, string>>();
+            package.Add(new KeyValuePair<string, string>("header", "primary-header"));
+            package.Add(new KeyValuePair<string, string>("menu-left", "primary-menu-left"));
+            return package;
+        }
     }
 }
+
+//idea..zkusit udelat slozitejsi strukturu ktera bude definovat i css pathy atd ..widgety
