718ce18ca46fa62490d1a30bf036010dafd175d4
baska <ondra.basista@gmail.com>
2017-06-07 23:02:40 +0200

    js update

--- Kamzik/Scripts/kamzikjs-director.js
+++ Kamzik/Scripts/kamzikjs-director.js
@@ -3,13 +3,27 @@
         this.createNewPage();
     }
 
-    loadAjaxDataToSend() {
+    loadAjaxDataToSend(object) {
 
+    }
+
+    mapData(key,value) {
+        let map = new Map();
+        map.set(key, value);
+        return map;
+    }
+    fetchData(identifier, value, hardtype) {
+        if (typeof identifier === "string" && (typeof value === "string" || typeof value === "number") && hardtype != null) {
+            identifier = identifier instanceof $ ? identifier = identifier : identifier = $('.' + identifier);
+        }
+        else {
+            console.log('bad code format');
+        }
     }
 
     createNewPage() {
         $('.kamzikjs_submit-Create-Page-Form').on('click', function (e) {
-           e.preventDefault();
+            e.preventDefault();
             $.ajax({
                 url: "/Kamzik/CreateAjaxPage",
                 type: "POST",
@@ -34,4 +48,4 @@
         });
     }
 
-}
+    }
